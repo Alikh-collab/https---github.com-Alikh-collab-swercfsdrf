ea3f73344d7a93960ede1f21412e63fc035c2109
Alikhan <chort445@gmail.com>
2025-02-28 23:16:22 +0300

    dfdf

---
+++ №зкщиы/js.js
@@ -0,0 +1,43 @@
+// for (let i = 2; i <= 32; i *= 2) {
+//     console.log(i);
+// }
+
+// let info = [
+//     'Alikhan ',
+//      15, 
+//     'Leskhan'
+// ]
+// console.log(info);
+
+
+// info.push(
+//  'Almaty',
+//  'Kurmangazy',
+// )
+
+// info.pop[4];
+
+// console.log(info);
+
+// let slice = info.slice(2, 4);
+
+// console.log(slice);
+
+// let numbes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let three = numbes.filter(num => num % 3 === 0);
+
+// console.log(three);
+
+
+// let carInfo = {
+//     marke: 'BMW',
+//     year: 2003, 
+//     model: 'M5 E60',
+//     color: 'black',
+//     details: ['rulo', 'sedushka', 'steklo', 'dveri', 'motor'],
+// }
+
+// console.log(carInfo);
+
+
